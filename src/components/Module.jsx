42e9b2be1dd3639ee571746c99e7a4ca8bb3c214
Zahra Rajabi <zahra.80.rajabi@gmail.com>
2024-06-22 02:24:16 +0330

    landscape modal height corrected

--- src/components/Module.jsx
+++ src/components/Module.jsx
@@ -70,7 +70,7 @@ function Module({ setOpenModal, modalData, night }) {
       className="fixed inset-0 z-50 flex items-center justify-center cursor-pointer overscroll-none backdrop-blur-sm"
     >
       <section
-        className={`w-full md:w-3/5 mx-4 md:mx-0 p-2 md:p-4 text-indigo-100 border rounded-lg shadow-md h-3/5 xl:h-4/6 xl:w-3/6 border-indigo-50/50 bg-zinc-900/70 ${
+        className={`w-full landscape:h-[85%] h-3/5 overflow-scroll md:w-4/5 mx-4 md:mx-0 p-2 md:p-4 text-indigo-100 border rounded-lg shadow-md xl:h-4/6 xl:w-3/6 border-indigo-50/50 bg-zinc-900/70 ${
           !night && "!bg-indigo-100 border-indigo-600/50"
         }`}
       >
